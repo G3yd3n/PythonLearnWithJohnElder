96378edab577b54cc61664841886332757d595d9
g3yd3n <soonteckl@outlook.com>
2020-06-24 23:01:04 +0800

    Added datatype practices

--- hello.py
+++ hello.py
@@ -1,2 +1,20 @@
-print ("Hello World!")
+#################################
+#Learn Python Coding in 2020
+#By g3yd3n
+
+#################################
+
+
+#clear the terminal screen
+#os.system('clear')
+
+first_name = 'John'
+age = 41
+fav_pizza = {
+	"Henry": "Hawaii",
+	"Golden": "Labrado",
+	"Messei": "Rodano"
+	}
+
+print (fav_pizza["Henry"])
 
